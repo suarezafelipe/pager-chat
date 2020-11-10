9db8125ed9267a561c354bbc8de5194aa4e4071b
Felipe Suarez <suarez.a.felipe@gmail.com>
2020-11-09 21:43:44 -0500

    Add babel and webpack configuration for react

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
