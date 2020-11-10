cd46b7bb6cb0cc4adaaa45c1d339c50161f05616
Felipe Suarez <suarez.a.felipe@gmail.com>
2020-11-09 22:21:11 -0500

    Add css modules support

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.module.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1> Hello world </h1>
       </div>
     );
