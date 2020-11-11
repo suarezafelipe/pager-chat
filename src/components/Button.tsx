ee2c8dd560e8f0d30a6c5861c76b453c10447792
Felipe Suarez <suarez.a.felipe@gmail.com>
2020-11-11 10:47:29 -0500

    Add active and disabled stlyes in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,24 @@ import classes from "./Button.module.css";
 interface Props {
   text: string;
   onClick: () => void;
+  isDisabled: boolean;
 }
 
-const Button: React.FC<Props> = ({ text, onClick }): JSX.Element => {
+const Button: React.FC<Props> = ({
+  text,
+  onClick,
+  isDisabled,
+}): JSX.Element => {
   return (
-    <div className={classes.Container}>
-      <button className={classes.Button} onClick={onClick}>{text}</button>;
-    </div>
+    <button
+      className={`${classes.Button} ${
+        isDisabled ? classes.Disabled : classes.Active
+      }`}
+      onClick={onClick}
+      disabled={isDisabled}
+    >
+      {text}
+    </button>
   );
 };
 
